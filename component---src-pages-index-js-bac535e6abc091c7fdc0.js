cd25663bb561c70b48cb14be2313203256e24fe7
laurakcaron <37113696+laurakcaron@users.noreply.github.com>
2021-03-18 21:14:12 -0400

    Updates

---
+++ component---src-pages-index-js-bac535e6abc091c7fdc0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),r=a.n(n),o=a("Bl7J"),i=a("Wbzz");t.default=function(e){var t=Object(i.d)("4224701694");return r.a.createElement(o.a,{pageTitle:"About",pageDescription:"Learn more about me"},r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.site.siteMetadata.about}}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-bac535e6abc091c7fdc0.js.map
