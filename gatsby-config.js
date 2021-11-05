f1f96cc315ba72c7fcfbf59dd74859e628c5595a
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2021-11-05 18:59:37 -0400

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -510,7 +510,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `../static/favicon.jpg`
+        icon: `${__dirname}/static/favicon.jpg`
       },
     },
   ],
