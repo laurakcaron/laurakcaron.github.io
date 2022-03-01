3b85030650bd848fe4aebcd8246d745d48d91598
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2022-03-01 16:53:14 -0500

    Update research card styles

--- src/components/link-cards.js
+++ src/components/link-cards.js
@@ -15,16 +15,18 @@ const LinkCard = props => {
     <div className="cardImage" style={{zIndex:"0", opacity:"1"}}>
    <Image filename={props.image} alt={props.alt} style={{width:"100%", opacity: "1"}}></Image>
    </div>
-   <div style={{padding:"4px"}}>
+ 
+</div>
+
+  <div className="second" style={{lineHeight:"1.1"}} aria-label={props.shorttitle}>
+  <div>
    <br></br>
    <h2>{props.shorttitle} ({props.date})<br></br></h2>
    <br></br>
    <h3>{props.authors}, </h3>
   <h3 style={{fontStyle:"italic"}}>{props.place}</h3>
   </div>
-</div>
-
-  <div className="second" style={{lineHeight:"1.1"}} aria-label={props.shorttitle}>
+    
     <p style={{fontSize:"smaller", color:"black", marginBottom:"0px", marginTop:"10px"}}>{props.abstract}</p>
     <br></br>
     <div className="button-outer" style={{marginTop:"0px", fontSize:"smaller"}}>
