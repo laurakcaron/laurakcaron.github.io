490a91638b3bcb380f6b8a172fc4d4c9dc1b974d
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2024-12-06 11:07:49 -0500

    update teaching

--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -16,13 +16,21 @@ export default props => {
 <h3 style={{lineHeight:"1.1", fontSize:"larger", marginBottom: "0px"}}>Teaching Awards</h3>
 <hr style={{margin:"1rem"}}></hr>
 <ul style={{listStyleType: "none"}}>
+<li>Wueller Teaching Award for Best TA in PhD Courses, AY 2023-2024, Columbia Department of Economics</li>
   <li>Wueller Teaching Award for Best TA in Undergraduate Core Courses, AY 2021-2022, Columbia Department of Economics</li>
+</ul>
+
+<h3 style={{lineHeight:"1.1", fontSize:"larger", marginBottom: "0px"}}>Instructor of Record</h3>
+<hr style={{margin:"1rem"}}></hr>
+<ul style={{listStyleType: "none"}}>
+  <li>Labor Economics, Summer 2024. Columbia University Department of Economics. Evaluation 4.5/5.  </li>
 </ul>
 
 <h3 style={{lineHeight:"1.1", fontSize:"larger", marginBottom: "0px"}}>Teaching Assistantships</h3>
 <hr style={{margin:"1rem"}}></hr>
 <ul style={{listStyleType: "none"}}>
-  <li>Political Economy, Fall 2023. Alessandra Casella, Columbia University Department of Economics. </li>
+  <li>PhD Econometrics, Spring 2024. Jushan Bai and Simon Lee, Columbia University Department of Economics. Evaluation 4.8/5.  </li>
+  <li>Political Economy, Fall 2023. Alessandra Casella, Columbia University Department of Economics. Evaluation 4.8/5. </li>
   <li>Intermediate Microeconomics, Spring 2022. Professor Ingmar Nyman, Columbia University Department of Economics. Evaluation 4.8/5. </li>
   <li>Political Economy, Fall 2021. Alessandra Casella, Columbia University Department of Economics. Evaluation 4.5/5. </li>
   <li>Macroeconomics, Spring 2017. Professor Carol Rogers, Georgetown University Department of Economics. Evaluation 4.1/5.</li>
