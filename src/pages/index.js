ecec4865f32a8fc1fb2649b67fe56b0466ea8e95
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2025-10-28 20:34:27 -0400

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default props => {
   return (
     <Layout pageTitle="About" pageDescription="Learn more about me">
       <div dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.about }} />
-      <p>You can <a href="Caron_CV_Sept2025.pdf">download my CV here</a>. </p>
+      <p>You can download my <a href="Caron_CV_Sept2025.pdf">CV here</a> and my <a href="Caron_Public_Education_Disabled_Students.pdf">JMP here</a>. </p>
 
       <p><a href="/research" >Learn more about my research here!</a></p>
     </Layout>
