e7938cb3e4ff7ae61b7b812aebcfe640cb3b4ae9
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2026-01-14 19:49:10 -0500

    update CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default props => {
   return (
     <Layout pageTitle="About" pageDescription="Learn more about me">
       <div dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.about }} />
-      <p>You can download my <a href="Caron_CV_Oct2025.pdf" target="blank">CV here</a> and my <a href="Caron_Public_Education_Disabled_Students.pdf" target="blank">JMP here</a>. </p>
+      <p>You can download my <a href="Caron_CV_Jan2026.pdf" target="blank">CV here</a> and my <a href="Caron_Public_Education_Disabled_Students.pdf" target="blank">JMP here</a>. </p>
 
       <p>Or watch a <a href ="https://www.youtube.com/watch?v=FZMdnobZvps" target="blank">5 minute video summary</a> of my JMP!</p>
 
