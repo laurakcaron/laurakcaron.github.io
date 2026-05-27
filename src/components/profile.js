1c344fac1054f8c81436b7f5227f461a545bf5ca
Laura Caron <37113696+laurakcaron@users.noreply.github.com>
2026-05-27 11:12:36 -0400

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -34,7 +34,7 @@ const Profile = props => {
         <h4 id="profile-name">{props.name}</h4>
         <h6 id="profile-role">{props.role}</h6>
         <p id="profile-email">
-          {/* she/her <br /> */}
+         they/them or ey/em <br /> 
           <a href={"mailto:" + props.email}>{props.email || ""}</a>
         </p>
       </div>
